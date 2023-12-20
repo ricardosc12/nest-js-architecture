79ff5e469d3b3b140c025730f9e271ed69183f70
Ricardo <ricardo.spinola9@gmail.com>
2023-12-20 12:28:18 -0300

    remove app controller & services

--- nest-project/src/app.module.ts
+++ nest-project/src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './routes/users/users.module';
 import { DatabaseModule } from './database/mongo/database.module';
 
 @Module({
 	imports: [UsersModule],
-	controllers: [AppController],
-	providers: [AppService],
+	controllers: [],
+	providers: [],
 })
 export class AppModule { }
